9ba84d783c310a8086b2e41073afa445583ead6d
wendy122 <44657611+wendy122@users.noreply.github.com>
2019-10-21 13:35:12 -0400

    Database connection files to C#
    Connects the database, allows queries such as table creation, insertion, and reading from the database. Very basic currently and simply prints testing

---
+++ Mission/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteDemo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = CreateConnection();
+            Console.WriteLine("Testing");
+            //CreateTable(sqlite_conn);
+            InsertData(sqlite_conn);
+            ReadData(sqlite_conn);
+        }
+
+        static SQLiteConnection CreateConnection()
+        {
+
+            SQLiteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source=Mission.db; Version = 3; New = True; Compress = True; ");
+            Console.WriteLine("Opened and connected");
+            // Open the connection:
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return sqlite_conn;
+        }
+        
+       static void CreateTable(SQLiteConnection conn)
+        {
+
+            SQLiteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE Description(CaseID INT, Name TEXT, Desc BLOB)";
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = Createsql;
+            sqlite_cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Create table command");
+        }
+        
+       static void InsertData(SQLiteConnection conn)
+        {
+           SQLiteCommand sqlite_cmd;
+           sqlite_cmd = conn.CreateCommand();
+           sqlite_cmd.CommandText = "INSERT INTO Description(Col1, Col2) VALUES('Test1 Text1 ', 2); ";
+           sqlite_cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Insert Data into Description Table");
+        }
+
+        
+        static void ReadData(SQLiteConnection conn)
+        {
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM Description";
+
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+            while (sqlite_datareader.Read())
+            {
+                string myreader = sqlite_datareader.GetString(0);
+                Console.WriteLine(myreader);
+            }
+            conn.Close();
+        }
+    } 
+}
